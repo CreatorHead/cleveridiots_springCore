2b4b1daa6c61a621b12934046f75dc698c94b45a
Blake <Blake@DESKTOP-A3TKUI6>
2019-07-20 00:55:18 -0700

    DI example 2

---
+++ src/main/java/com/dev/spring/core/beans/Car.java
@@ -0,0 +1,7 @@
+package com.dev.spring.core.beans;
+
+public class Car {
+	private String model;
+	private String brand;
+	private Engine engine;
+}
---
+++ src/main/java/com/dev/spring/core/config/CarConfig.java
@@ -0,0 +1,5 @@
+package com.dev.spring.core.config;
+
+public class CarConfig {
+
+}
